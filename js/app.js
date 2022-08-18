8b34eee8a7dcfe0d255108623457df19071093fb
Jeymun <santiagojeymun@gmail.com>
2022-08-18 16:03:14 -0300

    Creando

---
+++ js/app.js
@@ -0,0 +1,46 @@
+const dinero = 600
+const totalAPagar = 500
+
+if (dinero > totalAPagar) {
+    console.log("Podemos pagar")
+} else{
+    console.log("Fondos insuficientes")
+}
+
+const usuario = true;
+const puedePagar = false;
+
+if (usuario && puedePagar) {
+    console.log("Puedes comprar");
+} else if (usuario) {
+    console.log("No puedes comprar");
+} else if (puedePagar) {
+    console.log ("No puedes pagar");
+}
+
+const efectivo = 300
+const credito = 500
+const disponible = efectivo + credito   
+const totalAPagarse = 600
+
+if (
+    efectivo > totalAPagarse ||
+    credito > totalAPagarse ||
+    disponible > totalAPagarse
+    )   {
+        console.log("Podemos comprar")
+    }
+    else{
+        console.log("Fondos insuficientes")
+    } 
+
+let nombreAlumno = prompt ("Ingresa el nombre del alumno")
+
+let apellidoAlumno = prompt ("Ingresa el apellido del alumno")
+
+if (nombreAlumno !== "" && apellidoAlumno !== "") {
+    alert( "Nombre: " + nombreAlumno + " Apellido " + apellidoAlumno)
+}   else{
+    alert("No ingreso datos")
+}a
+
