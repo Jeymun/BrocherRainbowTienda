09559c4c8d833aa27b78e6e5df3ecd87489311e0
Jeymun <santiagojeymun@gmail.com>
2022-09-05 23:32:36 -0300

    Creando

--- js/app.js
+++ js/app.js
@@ -45,12 +45,12 @@
 // }a
 
 
-for (let i = 1; i <= 15; i++) {
-    if (i === 13){
-    continue;
-}
-    alert(`El valor de I es ${i}`)
-}
+// for (let i = 1; i <= 15; i++) {
+//     if (i === 13){
+//     continue;
+// }
+//     alert(`El valor de I es ${i}`)
+// }
 
 
 
@@ -67,3 +67,44 @@ const advertencia = function () {
 
 };
 
+const arrayA = ["Pancho", true, "Test"]
+
+console.log(arrayA[0])
+
+
+
+const nombres = ["Eula", "Xiao", "Ganyu", "Hu tao", "Willyrex"]
+
+const eliminar = (nombre) => {
+    let i = nombres.indexOf(nombre);
+
+    if (i != -1) {
+        nombres.splice(i, 1);
+    }
+}
+
+eliminar("Ganyu")
+
+console.log(nombres)
+
+
+const object1 = { id: 1, producto: "Figura"};
+
+const array = [object1, {id: 2, producto: "Almohada"}];
+
+array.push({ id: 3, producto: "Figura2"});
+
+console.log(array)
+
+
+
+const elementos = [
+    {id: 1, elemento: "Anemo" },
+    {id: 2, elemento: "Pyro" },
+    {id: 3, elemento: "Electro" },
+];
+
+for (const elemento of elementos){
+    console.log(elemento.id);
+    console.log(elemento.elemento);
+}
