5997e61645dcf4d3051c1d44438f1c35cdffac74
Jeymun <santiagojeymun@gmail.com>
2022-09-24 04:54:47 -0300

    cambios

--- js/app.js
+++ js/app.js
@@ -1,137 +1,24 @@
-// const dinero = 600
-// const totalAPagar = 500
 
-// if (dinero > totalAPagar) {
-//     console.log("Podemos pagar")
-// } else{
-//     console.log("Fondos insuficientes")
-// }
+const formulario = document.getElementsByClassName("caja--index");
 
-// const usuario = true;
-// const puedePagar = false;
+console.log(formulario);
 
-// if (usuario && puedePagar) {
-//     console.log("Puedes comprar");
-// } else if (usuario) {
-//     console.log("No puedes comprar");
-// } else if (puedePagar) {
-//     console.log ("No puedes pagar");
-// }
+const testdelete = document.querySelector(".testdelete");
 
-// const efectivo = 300
-// const credito = 500
-// const disponible = efectivo + credito   
-// const totalAPagarse = 600
+testdelete.remove();
 
-// if (
-//     efectivo > totalAPagarse ||
-//     credito > totalAPagarse ||
-//     disponible > totalAPagarse
-//     )   {
-//         console.log("Podemos comprar")
-//     }
-//     else{
-//         console.log("Fondos insuficientes")
-//     } 
+ let agregar = document.createElement("p")
+ agregar.innerHTML = "<h5>Test Dom</h5>";
+ document.body.appendChild(agregar);
 
-// let nombreAlumno = prompt ("Ingresa el nombre del alumno")
+ function comprarboton () {
+    document.getElementById("comprarBoton").innerHTML = "Agregaste al carrito";
+  };
 
-// let apellidoAlumno = prompt ("Ingresa el apellido del alumno")
+  function comprarboton1 () {
+    document.getElementById("comprarBoton1").innerHTML = "Agregaste al carrito";
+  };
 
-// if (nombreAlumno !== "" && apellidoAlumno !== "") {
-//     alert( "Nombre: " + nombreAlumno + " Apellido " + apellidoAlumno)
-// }   else{
-//     alert("No ingreso datos")
-// }a
-
-
-// for (let i = 1; i <= 15; i++) {
-//     if (i === 13){
-//     continue;
-// }
-//     alert(`El valor de I es ${i}`)
-// }
-
-
-
-let precio = function (a, b){
- return a * b;   
-};
-
-let resultado = precio(3, 6);
- 
-console.log(resultado);
-
-const advertencia = function () {
-    return "Peligro"
-
-};
-
-const arrayA = ["Pancho", true, "Test"]
-
-console.log(arrayA[0])
-
-
-
-const nombres = ["Eula", "Xiao", "Ganyu", "Hu tao", "Willyrex"]
-
-const eliminar = (nombre) => {
-    let i = nombres.indexOf(nombre);
-
-    if (i != -1) {
-        nombres.splice(i, 1);
-    }
-}
-
-eliminar("Ganyu")
-
-console.log(nombres)
-
-
-const object1 = { id: 1, producto: "Figura"};
-
-const array = [object1, {id: 2, producto: "Almohada"}];
-
-array.push({ id: 3, producto: "Figura2"});
-
-console.log(array)
-
-
-
-const elementos = [
-    {id: 1, elemento: "Anemo" },
-    {id: 2, elemento: "Pyro" },
-    {id: 3, elemento: "Electro" },
-];
-
-for (const elemento of elementos){
-    console.log(elemento.id);
-    console.log(elemento.elemento);
-}
-
-[
-    {
-    "name" : "Javascript",
-    "boolen": true,
-    "entregas": ["desafios","complementario", "preentregas"]
-    },
-
-];
-
-const button = document.querySelector("#amm");
-
-button.onclick = function () {
-    alert("Agregate al carrito");
-};
-
-const buton= document.querySelector("#asd");
-
-buton.onclick = function () {
-    alert("Dislike");
-};
-
-const buton1 = document.querySelector("#asado");
-
-buton1.onclick = function () {
-    alert("Asado de miel");
-};
+  function comprarBoton2 () {
+    document.getElementById("comprarBoton2").innerHTML = "Agregaste al carrito";
+  };
