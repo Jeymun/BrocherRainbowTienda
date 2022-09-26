3ae7af7bd99ab97ab9d0100c5a1532f7674ffc58
Jeymun <santiagojeymun@gmail.com>
2022-09-26 01:22:54 -0300

    Json

--- js/app.js
+++ js/app.js
@@ -22,3 +22,16 @@ testdelete.remove();
   function comprarBoton2 () {
     document.getElementById("comprarBoton2").innerHTML = "Agregaste al carrito";
   };
+
+
+
+const producto1 ={
+  titulo : "Nekopara",
+  precio : 39,
+  disponible: true
+}
+
+const producto1Str = JSON.stringify(producto1)
+
+localStorage.setItem("tienda" , producto1Str)
+
