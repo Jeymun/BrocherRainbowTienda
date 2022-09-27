ac81a47ec81c74744cb06cf3038d45ad57172564
Jeymun <santiagojeymun@gmail.com>
2022-09-27 17:44:12 -0300

    librerias

--- js/app.js
+++ js/app.js
@@ -19,19 +19,52 @@ testdelete.remove();
  agregar.innerHTML = "<h5>Test Dom</h5>";
  document.body.appendChild(agregar);
 
+
+// Operador
+
+
+
 // Botones de compra
 
- function comprarboton () {
-    document.getElementById("comprarBoton").innerHTML = "Agregaste al carrito";
-  };
+//  function comprarboton () {
+//     document.getElementById("comprarBoton").innerHTML = "Agregaste al carrito";
+//   };
 
-  function comprarboton1 () {
-    document.getElementById("comprarBoton1").innerHTML = "Agregaste al carrito";
-  };
+//   function comprarboton1 () {
+//     document.getElementById("comprarBoton1").innerHTML = "Agregaste al carrito";
+//   };
 
-  function comprarBoton2 () {
-    document.getElementById("comprarBoton2").innerHTML = "Agregaste al carrito";
-  };
+//   function comprarBoton2 () {
+//     document.getElementById("comprarBoton2").innerHTML = "Agregaste al carrito";
+//   };
 
 
 
+// Librerias
+
+const btn = document.querySelector("#comprarBoton")
+btn.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+
+const btn1 = document.querySelector("#comprarBoton1")
+btn1.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+
+const btn2 = document.querySelector("#comprarBoton2")
+btn2.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
